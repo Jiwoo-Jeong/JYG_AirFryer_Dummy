d0b63b59a7409fe55e3e8407ed6c9b5e7c9fcac4
Jiwoo-Jeong <jwjeong@yookgak.com>
2022-10-28 17:13:21 +0900

    change to flask-sock

--- app.py
+++ app.py
@@ -1,34 +1,17 @@
-from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-import ssl
-app = Flask(__name__)
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_sock import Sock
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+sock = Sock(app)
+
 
 @app.route('/')
 def index():
-   print('Request for index page rSeceived')
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
-if __name__ == '__main__':
-    socketio.run(app)
+@sock.route('/echo')
+def echo(sock):
+    while True:
+        data = sock.receive()
+        sock.send(data)
