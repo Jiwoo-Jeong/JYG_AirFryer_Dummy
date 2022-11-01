81ef10257ca11fa7a8397b74ab7c3b19cc7db3f9
Jiwoo-Jeong <jwjeong@yookgak.com>
2022-11-01 15:48:08 +0900

    change to flask only

--- app.py
+++ app.py
@@ -1,44 +1,21 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory ,make_response
 import ssl
-import asyncio
-import ssl
-import websockets
-import time
-import asyncio
-import threading
-
-
-async def echo(websocket):
-    async for message in websocket:
-        global log
-        log.append("["+time.asctime()+"] >>> "+message)
-        await websocket.send(message)
-        log.append("["+time.asctime()+"] <<< "+message)
-
-async def main2():
-    async with websockets.serve(echo, "localhost", 8765, ssl=ssl_context):
-        await asyncio.Future()  # run forever
-
-def run_ws():
-    print("ws server started")
-    asyncio.run(main2())
+import time , os
+from flask import request
 
 app = Flask(__name__)
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 ssl_context.load_cert_chain(certfile='cert.pem', keyfile='key.pem')
 
 global log
-log = ["["+time.asctime()+"] server started"]
-
-thread1 = threading.Thread(target=run_ws)
-thread1.daemon = True
-thread1.start()
+log = []
 
 @app.route('/')
 def index():
    print('Request for index page rSeceived')
    return render_template('index.html')
+
 
 @app.route('/favicon.ico')
 def favicon():
@@ -48,7 +25,6 @@ def favicon():
 @app.route('/hello', methods=['POST'])
 def hello():
    name = request.form.get('name')
-
    if name:
        print('Request for hello page received with name=%s' % name)
        return render_template('hello.html', name = name)
@@ -57,28 +33,23 @@ def hello():
        return redirect(url_for('index'))
 
 
-@app.route('/log', methods=['POST','GET'])
-def send_log():
-    global log
-    rt = ""
-    for l in log:
-        rt += l +"\n";
-    r = make_response(rt,200)
+@app.route('/ping', methods=['POST','GET'])
+def pong():
+    r = make_response("alive",200)
     r.mimetype = "text/plain"
     return r
 
-@app.route('/clear', methods=['POST','GET'])
+@app.route('/clear_log', methods=['POST','GET'])
 def clear():
     global log
     log = ["["+time.asctime()+"] log cleared!"]
     rt = ""
     for l in log:
-        rt += l +"\n";
+        rt += l +"\n"
     r = make_response(rt,200)
     r.mimetype = "text/plain"
     return r
 
 
-
 if __name__ == '__main__':
-    app.run(app,ssl_context=ssl_context)
+    app.run(app = app,ssl_context=ssl_context)
